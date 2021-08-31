517a9e5b2778e15c549097baad6d2e273ff2d709
TellurideOfGold <scienceprimo@aol.com>
2021-08-30 23:32:06 -0400

    Start of project euler.

---
+++ 1.hs
@@ -0,0 +1,12 @@
+import Data.List (transpose)
+import System.Environment
+
+-- Problem 1 asks us to find the sum of all the multiples of 3 and 5 below 1000. This program will find that sum for any positive bound.
+
+threesAndFives = concat . transpose $ [[3,18..], [5,20..], [6,21..], [9,24..], [10,25..], [12,27..], [15,30..]]
+
+main = do
+    a <- getArgs
+    let n = read (head a) :: Int
+    print . sum . takeWhile (< n) $ threesAndFives
+
