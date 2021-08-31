c07f64af47bf38608ae803280cd8a3bd09964a9a
TellurideOfGold <scienceprimo@aol.com>
2021-08-31 13:13:40 -0400

    Fixed a typo in a comment.

--- 1c.hs
+++ 1c.hs
@@ -10,7 +10,7 @@ sum1To n = quot (n * (n + 1)) 2
 -- Sum of multiples of m from m to n:
 sumMultiplesOfTo m n = m * sum1To (quot n m)
 
--- Add it all up; making sure to subtract out the duplicates:
+-- Add it all up, making sure to subtract out the duplicates:
 threesAndFivesToN n = let n' = n - 1 in (sumMultiplesOfTo 3 n') + (sumMultiplesOfTo 5 n') - (sumMultiplesOfTo 15 n')
 
 main = do
